98fea951e806a7ef75c8a2d4107a990d03258520
Soheib <soheib.biga@outlook.fr>
2018-12-14 11:49:30 +0100

    clear

--- GeoStation_Projet/apisncf.h
+++ GeoStation_Projet/apisncf.h
@@ -7,6 +7,15 @@ class ApiSncf : public AbstractApi
 {
 public:
     ApiSncf(int id);
+    QNetworkAccessManager *manager_prochain_depart;
+    QString code_uic_gare;
+    qint64 t1;
+    qint64 t2;
+
+private slots:
+    void result_liste_gare(QNetworkReply *reply);
+    void result_prochain_depart(QNetworkReply* reply);
+    void button_clicked();
 };
 
 #endif // APISNCF_H
