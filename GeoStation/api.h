c1c199d7a656c5dc074d4089350ad90ea0852ef5
Soheib <soheib.biga@outlook.fr>
2018-12-14 10:57:09 +0100

    MainWindow + Classe Mere Api

---
+++ GeoStation/api.h
@@ -0,0 +1,27 @@
+#ifndef API_H
+#define API_H
+
+#include <QObject>
+#include <QMap>
+#include <QNetworkAccessManager>
+
+class AbstractApi
+{
+public:
+    Api();
+
+protected:
+    //Correspond au formulaire que l'on veut afficher dans la widget
+    //Par exemple si l'on veut afficher dans la widget le titre et l'adresse du musée Grevin à savoir 10 Boulevard Montmartre, 75009 Paris
+    //On fera map_formulaire.add("Nom","Musée Grevin") et map_formulaire.add("Adresse","10 Boulevard Montmartre, 75009 Paris"
+    QMap<QString, QString> map_formulaire;
+    QNetworkAccessManager manager;
+    enum IdWidget {Evenement,Vegicrue,Avions,Meteo,Pollution,Musee,Pharmacie,BorneElectrique,Satellite,Geolocalisation,Sncf};
+
+    virtual IdWidget getIdWidget() = 0;
+
+
+
+};
+
+#endif // API_H
