c587c01bb183d42bf129d1d5a99df5caf01ccdbd
ajc98 <vanwynsberghe.gary@gmail.com>
2018-12-14 15:16:42 +0100

    Modification nom des classes pharmacie.

--- GeoStation_Projet/pharmacieapi.cpp
+++ GeoStation_Projet/pharmacieapi.cpp
@@ -2,14 +2,16 @@
 
 #include <QNetworkAccessManager>
 
-ApiPharmacie::ApiPharmacie(QObject *parent,int id):AbstractApi(parent, id)
+PharmacieApi::PharmacieApi(QObject *parent,int id):AbstractApi(parent, id)
 {
     manager = new QNetworkAccessManager(parent);
 
     latitude = 48.871671;
     longitude = 2.346106;
 
-    request.setUrl(QUrl("https://public.opendatasoft.com/api/records/1.0/search/?dataset=finess-etablissements&q=Pharmacie+d%27Officine&geofilter.distance=" + QString::number(latitude) + "%2C+" + QString::number(longitude) + "%2C+" + rayon));
+    request.setUrl(QUrl("https://public.opendatasoft.com/api/records/1.0/search/?dataset=finess-etablissements&q=Pharmacie+d%27Officine&geofilter.distance="
+                        + QString::number(latitude) + "%2C+" + QString::number(longitude) + "%2C+" + rayon));
+
     reply = manager->get(request);
 
     connect(reply, SIGNAL(readyRead()), this, SLOT(slotReadyRead()));
@@ -20,14 +22,11 @@ ApiPharmacie::ApiPharmacie(QObject *parent,int id):AbstractApi(parent, id)
 }
 
 
-void ApiPharmacie::listePharmacie(QNetworkReply *reply)
+void PharmacieApi::listePharmacie(QNetworkReply *reply)
 {
-
     QByteArray tab = reply->readAll();
 
     QJsonDocument doc = QJsonDocument::fromJson(tab);
-
-    qDebug() <<"alors"<< doc.object().toVariantMap().keys();
 
     QJsonArray tableau = doc.object().value("records").toArray();
 
@@ -39,21 +38,21 @@ void ApiPharmacie::listePharmacie(QNetworkReply *reply)
     QString codePostal = doc.object().toVariantMap()["records"].toJsonArray().at(i)["fields"].toObject()["ligneacheminement"].toString();
 
     //ui->textEdit->insertPlainText(nom + "\n" + adresse + "\n" + codePostal + "\nà " + distance + " mètres\n\n");
-    qDebug() << nom << "\n" << adresse << "\n" << codePostal << "\nà" << distance << " mètres\n\n";
+
     }
 }
 
-void ApiPharmacie::slotReadyRead()
+void PharmacieApi::slotReadyRead()
 {
-    qDebug() << "SlotReadyRead";
+
 }
 
-void ApiPharmacie::slotError(QNetworkReply::NetworkError)
+void PharmacieApi::slotError(QNetworkReply::NetworkError)
 {
-    qDebug() <<"slotError" ;
+
 }
 
-void ApiPharmacie::slotSslErrors(QList<QSslError>)
+void PharmacieApi::slotSslErrors(QList<QSslError>)
 {
-    qDebug() << "slotSslError";
+
 }
