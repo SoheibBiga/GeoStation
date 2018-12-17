5e35acf33eda4f2fddeabcf6ab10868cd905bbec
ppljajc <pljajc@outlook.com>
2018-12-17 16:14:37 +0100

    IWidget.hh

---
+++ GeoStation_Projet/IWidget.hh
@@ -0,0 +1,22 @@
+#ifndef			IWIDGET_HH_
+# define		IWIDGET_HH_
+
+# include <QLayout>
+
+class IWidget
+{
+	public:
+		virtual int					getId() const = 0;
+		virtual QLayout*		getMainLayout() const = 0;
+		virtual QLayout*		getMozaicLayout() const = 0;
+		virtual bool				contentMap() const = 0;
+		virtual bool				hasMozaicLayout() const = 0;
+		virtual bool				refresh() const = 0;
+		virtual bool				init() = 0;
+		virtual bool				run() = 0;
+		virtual bool				exec() = 0;
+		virtual bool				show() = 0;
+		virtual bool				end() = 0;
+};
+
+#endif // IWIDGET_HH
