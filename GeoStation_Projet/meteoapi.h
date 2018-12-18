126b847858838b607de47c7ce986e634886027d8
stef18 <olivierdoranges@hotmail.fr>
2018-12-18 09:53:01 +0100

    api meteo

---
+++ GeoStation_Projet/meteoapi.h
@@ -0,0 +1,25 @@
+#ifndef METEOAPI_H
+#define METEOAPI_H
+#include "abstractapi.h"
+#include <QNetworkReply>
+#include <QMainWindow>
+#include <QList>
+#include <QSslError>
+
+class MeteoApi : public AbstractApi
+{
+    Q_OBJECT
+
+public:
+    MeteoApi(QObject* parent = 0);
+
+private:
+    QNetworkReply *reply;
+
+public slots:
+    void Read(QNetworkReply *reply);
+    //void update();
+    //void error();
+};
+
+#endif // METEOAPI_H
