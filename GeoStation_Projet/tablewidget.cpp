d6774ac247064d5d5bee6e957bf813485e28f668
ghania <ghania.bara@yahoo.fr>
2018-12-18 12:56:27 +0100

    ajout methode refresh par Ghania

--- GeoStation_Projet/tablewidget.cpp
+++ GeoStation_Projet/tablewidget.cpp
@@ -48,9 +48,12 @@ bool TableWidget::refresh()
     if(map_formulaire->keys().size() == 0) return false;
     QTableWidgetItem *item =new QTableWidgetItem();
     QStringList vlabels;
+    int nb_row = map_formulaire->keys().size();
+    ui->tableWidget->setRowCount(nb_row);
+    ui->tableWidget->setColumnCount(1);
     int i = 0, j = 0;
-    for(i = 0; i < map_formulaire->keys().size(); i++){
-        for(j = 0; j < map_formulaire->keys().size(); j++){
+    for(i = 0; i < nb_row; i++){
+        for(j = 0; j < nb_row; j++){
             vlabels << map_formulaire->keys()[i];
             item ->setText(map_formulaire->value(map_formulaire->keys()[i]));
             ui->tableWidget->setItem(i, j, item);
