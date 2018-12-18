561c187ed123f86e85329557ed808d63e96e1033
ppljajc <pljajc@outlook.com>
2018-12-18 10:14:21 +0100

    full AWidget class

--- GeoStation_Projet/AWidget.cpp
+++ GeoStation_Projet/AWidget.cpp
@@ -1,6 +1,50 @@
 #include "AWidget.hh"
 
-AWidget::AWidget()
-{
+int	AWidget::currentId_ = 0;
 
+AWidget::AWidget()
+	: QWidget(),
+		id_(currentId_++),
+		mainLayout_(),
+		mozaicLayout_()
+{}
+
+AWidget::AWidget(QWidget* parent)
+	: QWidget(parent),
+		id_(currentId_++),
+		mainLayout_(),
+		mozaicLayout_()
+{}
+
+AWidget::~AWidget()
+{}
+
+AWidget::AWidget(AWidget const& other)
+	: QWidget(),
+		id_(currentId_++),
+		mainLayout_(),
+		mozaicLayout_()
+{
+	(void)other;
+}
+
+AWidget&			AWidget::operator=(AWidget const& other)
+{
+	(void)other;
+	return (*this);
+}
+
+int						AWidget::getId() const
+{
+	return (id_);
+}
+
+QLayout*			AWidget::getMainLayout() const
+{
+	return (mainLayout_);
+}
+
+QLayout*			AWidget::getMozaicLayout() const
+{
+	return (mozaicLayout_);
 }
--- GeoStation_Projet/AWidget.hh
+++ GeoStation_Projet/AWidget.hh
@@ -1,11 +1,36 @@
-#ifndef AWIDGET_HH
-#define AWIDGET_HH
+#ifndef				AWIDGET_HH_
+# define			AWIDGET_HH_
 
+# include <QWidget>
+# include "IWidget.hh"
 
-class AWidget
+class AWidget : public IWidget, public QWidget
 {
-	public:
+		static int					currentId_;
+
 		AWidget();
+		AWidget(AWidget const& other);
+		AWidget&			operator=(AWidget const& other);
+
+	protected:
+		int									id_;
+		QLayout*						mainLayout_;
+		QLayout*						mozaicLayout_;
+
+	public:
+
+		explicit AWidget(QWidget* parent = 0);
+		~AWidget();
+
+		virtual int					getId() const = 0;
+		virtual QLayout*		getMainLayout() const;
+		virtual QLayout*		getMozaicLayout() const;
+		virtual bool				contentMap() const = 0;
+		virtual bool				hasMozaicLayout() const = 0;
+		virtual bool				refresh() const = 0;
+		virtual bool				init() = 0;
+		virtual bool				run() = 0;
+		virtual bool				end() = 0;
 };
 
 #endif // AWIDGET_HH
