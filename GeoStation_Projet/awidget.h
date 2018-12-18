5bcf94e525c227464f1665a4bab4b2b523e28d3a
ppljajc <pljajc@outlook.com>
2018-12-18 16:22:06 +0100

    [ PAUL ] : updated awidget.* and remove iwidget.h (conflict heritage diamand)

--- GeoStation_Projet/awidget.h
+++ GeoStation_Projet/awidget.h
@@ -1,39 +1,41 @@
-#ifndef				AWIDGET_HH_
-# define			AWIDGET_HH_
+#ifndefAWIDGET_HH_
+# defineAWIDGET_HH_
 
 # include <QWidget>
-# include "iwidget.h"
+
+/*
+**AWidget::mainLayout_ and AWidget::mozaicLayout_ to instanciate in the
+** init() method of the derived class.
+** AWidget::id_ is setted with currentId_ which is auto-increment
+*/
 
 class AWidget : public QWidget
 {
-		static int					currentId_;
+  AWidget();
+  AWidget(AWidget const& other);
+  AWidget&operator=(AWidget const& other);
+  
+ protected:
+  int			id_;
+  QLayout*		mainLayout_;
+  QLayout*		mozaicLayout_;
 
-		AWidget();
-		AWidget(AWidget const& other);
-		AWidget&			operator=(AWidget const& other);
+  static int		currentId_;
 
-	protected:
-		int									id_;
-		QLayout*						mainLayout_;
-		QLayout*						mozaicLayout_;
+ public:
+  explicit AWidget(QWidget* parent = 0);
+  virtual ~AWidget();
 
-	public:
-
-		explicit AWidget(QWidget* parent = 0);
-		~AWidget();
-
-//		virtual int					getId() const;
-//		virtual QLayout*		getMainLayout() const;
-//		virtual QLayout*		getMozaicLayout() const;
-//		virtual bool				contentMap() const = 0;
-//		virtual bool				hasMozaicLayout() const = 0;
-//		virtual bool				refresh() const = 0;
-//		virtual bool				init() = 0;
-//		virtual bool				run() = 0;
-//		virtual bool				end() = 0;
-
-//public slots:
-   // virtual void send_info(QMap<QString, QString> *map_formulaire) =0;
+  virtual int		getId() const;
+  virtual QLayout*	getMainLayout() const;
+  virtual QLayout*	getMozaicLayout() const;
+  virtual bool		contentMap() const = 0;
+  virtual bool		hasMozaicLayout() const = 0;
+  virtual bool		refresh() const = 0;
+  virtual bool		init() = 0;
+  virtual bool		run() = 0;
+  virtual bool		end() = 0;
+  virtual void		addWidget(QWidget* widget) = 0;
 };
 
-#endif // AWIDGET_HH
+#endif // AWIDGET_HH_
