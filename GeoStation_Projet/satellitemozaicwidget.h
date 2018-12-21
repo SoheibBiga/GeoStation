2d012d930e8aa132e04ec8ec2c636ca8fd021838
DaviJam <daori971@gmail.com>
2018-12-21 10:36:35 +0100

    Ajout satellite

--- GeoStation_Projet/satellitemozaicwidget.h
+++ GeoStation_Projet/satellitemozaicwidget.h
@@ -2,6 +2,8 @@
 #define SATELLITEMOZAICWIDGET_H
 
 #include <QWidget>
+#include <QMap>
+#include "QDebug"
 
 namespace Ui {
 class SatelliteMozaicWidget;
@@ -15,8 +17,15 @@ public:
     explicit SatelliteMozaicWidget(QWidget *parent = 0);
     ~SatelliteMozaicWidget();
 
+
 private:
     Ui::SatelliteMozaicWidget *ui;
+
+public slots:
+    //void receive_info(QMap<QString,QString> map_formulaire);
+    void receive_info2(QMap<QString, QVariant> map_ameliore);
+
+
 };
 
 #endif // SATELLITEMOZAICWIDGET_H
