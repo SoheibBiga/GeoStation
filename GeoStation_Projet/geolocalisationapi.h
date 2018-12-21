dbe00501635591924cb706ee66b3807cdce2d3f3
Pokeko <mdardakh@student.42.fr>
2018-12-21 12:40:25 +0100

    Ajout de GeolocalisationWidget

--- GeoStation_Projet/geolocalisationapi.h
+++ GeoStation_Projet/geolocalisationapi.h
@@ -6,10 +6,13 @@
 class GeolocalisationApi : public AbstractApi
 {
 public:
-    GeolocalisationApi(ordonnanceur *ord, QObject *parent = 0);
+    GeolocalisationApi(ordonnanceur *ord, QObject *parent = nullptr);
     ~GeolocalisationApi();
 private slots:
     void    get_response(QNetworkReply *reply);
+    void    get_map(QNetworkReply *rep);
+private:
+    QMap<QString,QVariant> element;
 };
 
 #endif // GEOLOCALISATIONAPI_H
