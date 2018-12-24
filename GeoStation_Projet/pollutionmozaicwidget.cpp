19a1640de571144fc20d97d64a00da79f4267b04
theva <theva9999@gmail.com>
2018-12-24 14:29:23 +0100

    widget pollution a ajouter

--- GeoStation_Projet/pollutionmozaicwidget.cpp
+++ GeoStation_Projet/pollutionmozaicwidget.cpp
@@ -16,4 +16,38 @@ PollutionMozaicWidget::~PollutionMozaicWidget()
 void PollutionMozaicWidget::receive_info2(QMap<QString, QVariant> map_ameliore)
 {
     //ui->tableWidget->refresh_ameliore(map_ameliore);
+    int n = map_ameliore["Tableau"].toList().at(0).toMap()["AQI"].toInt();
+    if(n<=50){
+        ui->textEdit->setStyleSheet("background:#00e400;color:black");
+        ui->textEdit->setAlignment(Qt::AlignCenter);
+        //ui->textEdit->setFont(m_font);
+        ui->textEdit->setText(QString("Indice de qualité de l'air :\t\n%1\nBon!!").arg(QString::number(n)));
+    }else if (n<=100) {
+        ui->textEdit->setStyleSheet("background:#ff0;color:black");
+        ui->textEdit->setAlignment(Qt::AlignCenter);
+        //ui->textEdit->setFont(m_font);
+        ui->textEdit->setText(QString(" Indice de qualité de l'air :\t\n%1\nModéré!!").arg(QString::number(n)));
+     }else if (n<=150) {
+        ui->textEdit->setStyleSheet("background:#ff7e00;color:black");
+        //ui->textEdit->setFont(m_font);
+        ui->textEdit->setText(QString(" Indice de qualité de l'air :\t\n%1\nMauvais\npour les groupes\nsensibles!!").arg(QString::number(n)));
+     }else if (n<=200) {
+        ui->textEdit->setStyleSheet("background:#f00;color:white");
+        //ui->textEdit->setFont(m_font);
+        ui->textEdit->setText(QString(" Indice de qualité de l'air :\t\n%1\nMauvais!!").arg(QString::number(n)));
+     }else if (n<=300) {
+        ui->textEdit->setStyleSheet("background:#99004c;color:white");
+        //ui->textEdit->setFont(m_font);
+        ui->textEdit->setText(QString(" Indice de qualité de l'air :\t\n%1\nTrès mauvais!!").arg(QString::number(n)));
+     }else{
+        ui->textEdit->setStyleSheet("background:#7e0023;color:white");
+        //ui->textEdit->setFont(m_font);
+        ui->textEdit->setText(QString(" Indice de qualité de l'air :\t\n%1\nDangereux!!").arg(QString::number(n)));
+     }
+
+
+    ui->label->setStyleSheet("background:#b6b6b6");
+    //ui->label2->setFont(t_font);
+    ui->label2->setStyleSheet("background:#b6b6b6;color:#423b41");
+    ui->label2->setText(QString("POLLUTION"));
 }
