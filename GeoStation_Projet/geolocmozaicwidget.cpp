14930e88f51b61dd3b8c14f00148cec56ea874af
Pokeko <mdardakh@student.42.fr>
2018-12-24 10:28:13 +0100

    update geolocmozaicwidget

--- GeoStation_Projet/geolocmozaicwidget.cpp
+++ GeoStation_Projet/geolocmozaicwidget.cpp
@@ -7,11 +7,6 @@ GeoLocMozaicWidget::GeoLocMozaicWidget(QWidget *parent) :
     ui(new Ui::GeoLocMozaicWidget)
 {
     ui->setupUi(this);
-    PokeMap *map = new PokeMap(PokeMap::CENT_M);
-
-    map->addText(15, 40, "6 rue rougemont");
-    map->addPoint("48.871554", "2.346000");
-    ui->label->setPixmap(map->pixmap().scaled(this->size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
 
 }
 
@@ -28,19 +23,4 @@ void GeoLocMozaicWidget::receive_info(QMap<QString, QString> map_formulaire)
 void GeoLocMozaicWidget::receive_info2(QMap<QString, QVariant> map_ameliore)
 {
     //ui->tableWidget->refresh_ameliore(map_ameliore);
-}
-
-void    GeoLocMozaicWidget::setTextButton()
-{
-    //ui->label->setText("CHIEHCIHECIHEICHEI");
-}
-
-void GeoLocMozaicWidget::on_pushButton_clicked()
-{
-    ui->label->setText("Chicken");
-}
-
-void GeoLocMozaicWidget::on_pushButton_2_clicked()
-{
-    ui->label->setText("Chickkjfklsmfsdkfjdsfjdsjfmdsfjldsjfklmsdkjfmsdkfmsdkflmsdklfdklfmsdkmfskdlmfksdlmfskdmlfkdsl");
 }
