d1e66811d8bb433d6e9f2f896cca62d21ea7ffb8
ajc152 <mcoulont@yahoo.fr>
2018-12-26 17:49:52 +0100

    défilement musées

--- GeoStation_Projet/museesmozaicwidget.cpp
+++ GeoStation_Projet/museesmozaicwidget.cpp
@@ -26,43 +26,51 @@ void MuseesMozaicWidget::receive_info2(QMap<QString, QVariant> map_ameliore)
         ui->label_nomMusee->setAlignment(Qt::AlignCenter);
 
         if ( map_ameliore.contains("adresse")  ){
+            ui->gauche_adresse->setVisible(true) ;
+            ui->droite_adresse->setVisible(true) ;
             ui->gauche_adresse->setPixmap(QPixmap(QString::fromUtf8(":/Icons/adresse.svg")));
             ui->droite_adresse->setText ( map_ameliore["adresse"].toString());
             ui->droite_adresse->setWordWrap(true);
         }
         else {
-            ui->gauche_adresse->hide() ;
-            ui->droite_adresse->hide() ;
+            ui->gauche_adresse->setVisible(false) ;
+            ui->droite_adresse->setVisible(false) ;
         }
 
         if ( map_ameliore.contains("horaires") ){
+            ui->gauche_horaires->setVisible(true) ;
+            ui->droite_horaires->setVisible(true) ;
             ui->gauche_horaires->setPixmap(QPixmap(QString::fromUtf8(":/Icons/horaires.svg")));
             ui->droite_horaires->setText ( map_ameliore["horaires"].toString());
             ui->droite_horaires->setWordWrap(true);
         }
         else {
-            ui->gauche_horaires->hide() ;
-            ui->droite_horaires->hide() ;
+            ui->gauche_horaires->setVisible(false) ;
+            ui->droite_horaires->setVisible(false) ;
         }
 
         if ( map_ameliore.contains("fermeture") ){
+            ui->gauche_fermeture->setVisible(true) ;
+            ui->droite_fermeture->setVisible(true) ;
             ui->gauche_fermeture->setPixmap(QPixmap(QString::fromUtf8(":/Icons/fermeture.svg")));
             ui->droite_fermeture->setText ( map_ameliore["fermeture"].toString());
             ui->droite_fermeture->setWordWrap(true);
         }
         else {
-            ui->gauche_fermeture->hide() ;
-            ui->droite_fermeture->hide() ;
+            ui->gauche_fermeture->setVisible(false) ;
+            ui->droite_fermeture->setVisible(false) ;
         }
 
         if ( map_ameliore.contains("nocturnes") ){
+            ui->gauche_nocturnes->setVisible(true) ;
+            ui->droite_nocturnes->setVisible(true) ;
             ui->gauche_nocturnes->setPixmap(QPixmap(QString::fromUtf8(":/Icons/nocturnes.svg")));
             ui->droite_nocturnes->setText ( map_ameliore["nocturnes"].toString());
             ui->droite_nocturnes->setWordWrap(true);
         }
         else {
-            ui->gauche_nocturnes->hide() ;
-            ui->droite_nocturnes->hide() ;
+            ui->gauche_nocturnes->setVisible(false) ;
+            ui->droite_nocturnes->setVisible(false) ;
         }
 
         // Affichage de la photo :
