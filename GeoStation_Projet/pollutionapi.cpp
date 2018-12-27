82b13f55731a98e0bc1dc03c5c6670c79a83f296
theva <theva9999@gmail.com>
2018-12-27 11:42:16 +0100

    ok

--- GeoStation_Projet/pollutionapi.cpp
+++ GeoStation_Projet/pollutionapi.cpp
@@ -4,7 +4,12 @@ PollutionApi::PollutionApi(ordonnanceur *ord_, QObject *parent) : AbstractApi(or
 {
     manager = new QNetworkAccessManager(parent);
 
-    manager->get(QNetworkRequest(QUrl("https://api.waqi.info/feed/geo:48.866667;2.333333/?token=1427ed34fa11aff3521e1967be850e08e1df97cb")));
+    QString latitude = "48.866667";
+    latitude.toDouble();
+    QString longitude = "2.333333";
+    longitude.toDouble();
+
+    manager->get(QNetworkRequest(QUrl("https://api.waqi.info/feed/geo:"+latitude+";"+longitude+"/?token=1427ed34fa11aff3521e1967be850e08e1df97cb")));
 
     connect(manager,SIGNAL(finished(QNetworkReply*)),this,SLOT(replyFinished(QNetworkReply*)));
 }
