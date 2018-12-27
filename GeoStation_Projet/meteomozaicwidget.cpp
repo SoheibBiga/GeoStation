48309525dbcd763d9af95fb8cfa5554ff5571d5f
stef18 <olivierdoranges@hotmail.fr>
2018-12-27 13:22:19 +0100

    meteo ok modif

--- GeoStation_Projet/meteomozaicwidget.cpp
+++ GeoStation_Projet/meteomozaicwidget.cpp
@@ -1,6 +1,7 @@
 #include "meteomozaicwidget.h"
 #include "ui_meteomozaicwidget.h"
 #include <QDebug>
+#include <QtGui>
 
 MeteoMozaicWidget::MeteoMozaicWidget(QWidget *parent) :
     QWidget(parent),
@@ -16,13 +17,31 @@ MeteoMozaicWidget::~MeteoMozaicWidget()
 
 void MeteoMozaicWidget::receive_info2(QMap<QString, QVariant> map_ameliore)
 {
-    ui->labTmax_4->setText(map_ameliore["Tableau"].toList().at(0).toMap().value("Temperature Maximale").toString());
-    ui->labeTmin_4->setText(map_ameliore["Tableau"].toList().at(0).toMap().value("Temperature Minimale").toString());
+    double max = map_ameliore["Tableau"].toList().at(0).toMap().value("Temperature Maximale").toDouble() - 273.15;
+    double min = map_ameliore["Tableau"].toList().at(0).toMap().value("Temperature Minimale").toDouble() - 273.15;
+
+    ui->labTmax_4->setText(QString::number(max)+ "°");
+    ui->labeTmin_4->setText(QString::number(min)+ "°");
 
         // alignement centre
-        ui->labePre_4->setAlignment(Qt::AlignHCenter);
+        //ui->labePre_4->setAlignment(Qt::AlignHCenter);
         ui->labeTmin_4->setAlignment(Qt::AlignHCenter);
         ui->labTmax_4->setAlignment(Qt::AlignHCenter);
         ui->label_16->setAlignment(Qt::AlignHCenter);
-        ui->labDate_4->setAlignment(Qt::AlignHCenter);
+       // ui->labDate_4->setAlignment(Qt::AlignHCenter);
+
+
+
+
+    QLabel w;
+
+    // On crée une vidéo. Ici, c'est un GIF
+    QMovie movie("/home/stef18/Images/beta/BlissfulJoyousFanworms-size_restricted.gif");
+    // On l'associe à un label
+    w.setMovie (&movie);
+    // On lance la vidéo
+    movie.start ();
+
+    w.show();
+
 }
